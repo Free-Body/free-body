875643cbdaabcabf124f342996aba5462e9ad41a
Jessica Rogado <j.rogado17@yahoo.com>
2023-06-29 08:44:53 -0700

    added features folder

---
+++ .history/my-app/src/index/navbar_20230629084132.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+
+  return (
+    <div>
+      <nav>
+      <link rel="icon"  href="../public/logo.png"/>
+          <div>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/meet">Flowers</Link>
+            <Link to="/cart">Cart</Link>
+          </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ .history/my-app/src/index/navbar_20230629084145.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+
+  return (
+    <div>
+      <nav>
+      <link rel="icon"  href="../public/logo.png"/>
+          <div>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/meettheteam">Meet The Team</Link>
+            <Link to="/howitworks">Cart</Link>
+          </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
