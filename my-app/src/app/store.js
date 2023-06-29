e893807147525e05d780a819d6b519d2ca2c5b22
Jessica Rogado <j.rogado17@yahoo.com>
2023-06-29 09:25:05 -0700

    added app folder, index.js, but have an issue with'app.listen is not a func

---
+++ my-app/src/app/store.js
@@ -0,0 +1,12 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
+const middleware = [...getDefaultMiddleware(), logger];
+
+const store = configureStore({
+  reducer: {
+
+  },
+  middleware: middleware,
+});
+
+export default store;
--- my-app/src/features/navbar.js
+++ my-app/src/features/navbar.js
@@ -1,20 +1,18 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-
   return (
     <div>
       <nav>
-      <link rel="icon"  href="../public/logo.png"/>
-          <div>
-            <Link to="/home">Home</Link>
-            <Link to="/about">About</Link>
-            <Link to="/meettheteam">Meet The Team</Link>
-            <Link to="/howitworks">How It Works</Link>
-            <Link to="/activityland">Activity Land</Link>
-          </div>
+        <link rel="icon" href="../public/logo.png" />
+        <div>
+          <Link to="/home">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/meettheteam">Meet The Team</Link>
+          <Link to="/howitworks">How It Works</Link>
+          <Link to="/activityland">Activity Land</Link>
+        </div>
       </nav>
     </div>
   );
