975db6ffadf9eb73f4750fa48db984f4e998c03b
Ashley Ware <ashley.met@gmail.com>
2023-06-29 16:24:35 -0400

    more routing

---
+++ my-app/src/features/LearnAbout/buoyancy.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Buoyancy = () => {
+  return (
+    <div>
+      <h1>Learn about Buoyancy</h1>
+      <p>This is the content of learning about buoyancy.</p>
+    </div>
+  );
+};
+
+export default Buoyancy;
