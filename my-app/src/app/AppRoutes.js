bad88997734a9a995b270dcb8e0c33cfbb892740
Ashley Ware <ashley.met@gmail.com>
2023-06-29 16:17:20 -0400

    basic routing

--- my-app/src/app/AppRoutes.js
+++ my-app/src/app/AppRoutes.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "../features/homePage";
+import About from "../features/aboutPage";
+import Meet from "../features/meetTheTeam";
+import How from "../features/howItWorks";
+import Activity from "../features/activityLand";
 
 const AppRoutes = () => {
   return (
     <div>
       <Routes>
-        {/* Define your routes here */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/meettheteam" element={<Meet />} />
+        <Route path="/howitworks" element={<How />} />
+        <Route path="/activityland" element={<Activity />} />
       </Routes>
     </div>
   );
---
+++ my-app/src/features/meetTheTeam.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Meet = () => {
+  return (
+    <div>
+      <h1>Welcome to the Meet the Team Page</h1>
+      <p>This is the content of the Meet the Team Page.</p>
+    </div>
+  );
+};
+
+export default Meet;
