f4c8e891cd415a4dd2535055fa1ddff4077de1a4
Jessica Rogado <j.rogado17@yahoo.com>
2023-06-30 11:52:54 -0700

    added team the team images

--- my-app/src/features/meetTheTeam.js
+++ my-app/src/features/meetTheTeam.js
@@ -1,12 +1,40 @@
 import React from "react";
+import charlotteImage from "./images/charlotte.png";
+import jessicaImage from "./images/jessica.png";
+import ashleyImage from "./images/ashley.png";
+import carinaImage from "./images/carina.png";
 
-const Meet = () => {
+const Home = () => {
+  const handleLinkClick = (url) => {
+    window.open(url, "_blank");
+  };
+
   return (
     <div>
-      <h1>Welcome to the Meet the Team Page</h1>
-      <p>This is the content of the Meet the Team Page.</p>
+      <div className='header'>
+        <p id='line1'>Meet The Team</p>
+        <p id='line2'>click on the image to connect linkedin</p>
+      </div>
+      <div className='home-pics'>
+        <div className="person-card" onClick={() => handleLinkClick("https://www.linkedin.com/in/charlotte-currey/")}>
+          <img src={charlotteImage} alt='Charlotte' id='charlotte.png' />
+          <p className="person-name">Charlotte Currey</p>
+        </div>
+        <div className="person-card" onClick={() => handleLinkClick("https://www.linkedin.com/in/j-rogado/")}>
+          <img src={jessicaImage} alt='Jessica' id='jessica.png' />
+          <p className="person-name">Jessica Rogado</p>
+        </div>
+        <div className="person-card" onClick={() => handleLinkClick("https://www.linkedin.com/in/ashleynware/")}>
+          <img src={ashleyImage} alt='Ashley' id='ashley.png' />
+          <p className="person-name">Ashley Ware</p>
+        </div>
+        <div className="person-card" onClick={() => handleLinkClick("https://www.linkedin.com/in/carina-carlos/")}>
+          <img src={carinaImage} alt='Carina' id='carina.png' />
+          <p className="person-name">Carina Carlos</p>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Meet;
+export default Home;
