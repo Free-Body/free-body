ea52c77a52f3bf67f5e3ccef06d3671a4659d54b
Ashley Ware <ashley.met@gmail.com>
2023-06-30 11:59:06 -0400

    images not rendering

--- my-app/src/features/howItWorks.js
+++ my-app/src/features/howItWorks.js
@@ -5,6 +5,10 @@ const How = () => {
     <div>
       <h1>Welcome to the How it Works Page</h1>
       <p>This is the content of the How it Works Page.</p>
+      <img src="./images/boythrowing.png" alt="boy throwing ball"></img>
+      <img src="" alt="buoyancy"></img>
+      <img src="" alt="speedometer"></img>
+      <img src="" alt="circuitry"></img>
     </div>
   );
 };
