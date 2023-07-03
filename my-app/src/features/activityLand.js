5b758f71cecef4eef32956ea9955d2d6596d61b9
Ashley Ware <ashley.met@gmail.com>
2023-07-03 15:53:39 -0400

    activity land clickable images

--- my-app/src/features/activityLand.js
+++ my-app/src/features/activityLand.js
@@ -1,12 +1,48 @@
 import React from "react";
-
-const Activity = () => {
+// import { Link } from "react-router-dom";
+//for now the images link to home page until we have game links
+function InteractiveImage() {
   return (
     <div>
-      <h1>Welcome to Activity Land</h1>
-      <p>This is the content of Activity Land.</p>
+      <img
+        src={require("./images/physicsuniversity.png")}
+        alt="Interactive"
+        useMap="#image-map"
+        style={{
+          width: "80%",
+          height: "auto",
+          float: "right",
+          marginTop: "-34%",
+        }}
+      />
+      <map name="image-map">
+        <area
+          shape="rect"
+          coords="x274,y178,x74,y40"
+          href="../features/HomePage/homePage"
+          alt="Projectile Game"
+        />
+        <area
+          shape="rect"
+          coords="x710,y73,x914,y196"
+          href="../features/HomePage/homePage"
+          alt="Acceleration Game"
+        />
+        <area
+          shape="rect"
+          coords="x256,y642,x93,y478"
+          href="../features/HomePage/homePage"
+          alt="Buoyancy Game"
+        />
+        <area
+          shape="rect"
+          coords="x735,y532,x878,y670"
+          href="../features/HomePage/homePage"
+          alt="Circuitry Game"
+        />
+      </map>
     </div>
   );
-};
+}
 
-export default Activity;
+export default InteractiveImage;
