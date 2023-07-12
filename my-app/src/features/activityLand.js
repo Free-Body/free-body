4851509a37a3155819f08d8c6265d01920437de3
Jessica Rogado <j.rogado17@yahoo.com>
2023-07-12 08:39:40 -0700

    images on top should not change with screensize now

--- my-app/src/features/activityLand.js
+++ my-app/src/features/activityLand.js
@@ -9,14 +9,14 @@ function InteractiveImage() {
     <div className="activityLandContainer">
       <div id="activityLandDiv" style={{ position: "relative" }}>
         <img src={FreeBodyImage} alt="Interactive" />
-        <a href="/pizzatoss" style={{ position: "absolute", top: "0", left: "0" }}>
+        <a href="/pizzatoss" style={{ position: "absolute", top: "0%", left: "-1%" }}>
           <img id="pizzacar" src={PizzacarImage} alt="Pizza Car" style={{ width: "15vw" }} />
         </a>
-        <a href="/2fast2furious" style={{ position: "absolute", top: "0", right: "0", transform: "translate(-330px, 20px)" }}>
+        <a href="/2fast2furious" style={{ position: "absolute", top: "5%", right: "25%" }}>
           <img id="car" src={CarImage} alt="2 Fast 2 Furious" style={{ width: "15vw" }} />
         </a>
-        <a href="/freefloating" style={{ position: "absolute", bottom: "0", left: "0", transform: "translate(23px, -49px)" }}>
-          <img id="buoy" src={BuoyImage} alt="Buoy" style={{ width: "13vw" }} />
+        <a href="/freefloating" style={{ position: "absolute", bottom: "12%", left: "4%" }}>
+          <img id="buoy" src={BuoyImage} alt="Buoy" style={{ width: "11vw" }} />
         </a>
       </div>
     </div>
