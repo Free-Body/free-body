478efc5ccd3adea805db38868f235a2a128e617f
Jessica Rogado <j.rogado17@yahoo.com>
2023-07-14 09:08:29 -0700

    both pizza toss are done

--- my-app/src/Games/pizzaChallengeComponent.js
+++ my-app/src/Games/pizzaChallengeComponent.js
@@ -1,52 +1,97 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Challenge } from "./pizzaToss";
 import { init } from "./pizzaToss";
 import { Link } from "react-router-dom";
 
 const PizzaChallenge = () => {
-    useEffect(() => {
-        window.onload=init();
-    }, [])
-    
-    return (
+  const [showHint, setShowHint] = useState(false);
+
+  useEffect(() => {
+    window.onload = init();
+  }, []);
+
+  const handleHintMouseEnter = () => {
+    setShowHint(true);
+  };
+
+  const handleHintMouseLeave = () => {
+    setShowHint(false);
+  };
+
+  return (
     <div className="pizzaToss">
-        <h2>Pizza Toss - CHALLENGE MODE</h2>
-        <h3>Looks like rats have taken over the kitchen! Toss them some pizzas to lure them out!</h3>
-        <ol>Directions: 
-            <li>Choose a starting velocity using the sliders</li>
-            <li>You can still change the mass of the pizza and the gravity if you would like</li>
-            <li>When all the rats have fallen off the platform, you win!</li>
-        </ol>
-        <ol>Hints: 
-            <li>Velocity is a vector! How do we get the sum of two vectors?</li>
-            <li>Mass doesn't affect projectile motion, but size helps with knocking down more things!</li>
-        </ol>
-        <ol className="questions"> Think like a scientist:
-            <li>Keep the y velocity constant and change the x velocity. How does the x velocity change your path?</li>
-            <li>Keep the x velocity constant and change the y velocity. How does the y velocity change the path?</li>
-            <li>Change both velocities. How can you best approach optimizing the path?</li>
-            <li>How do different magnitudes of gravity affect your path?</li>
-            <li>How can you tie the above information to the equations you know?</li>
-        </ol>
-        <div id="areaToRender"></div>
-        <div>
-            <label htmlFor="xvelocityRange">X Velocity: <span id="xvelocityVal">10</span> m/s</label>
-            <input type="range" id="xvelocityRange" min="10" max="40" step="5"  defaultValue="15"/>
-
-            <label htmlFor="yvelocityRange">Y Velocity: <span id="yvelocityVal">10</span> m/s</label>
-            <input type="range" id="yvelocityRange" min="1" max="40" step="5"  defaultValue="15"/>
-
-            <label htmlFor="massRange">Mass: <span id="massVal">50</span> grams</label>
-            <input type="range" id="massRange" min="1" max="50" step="10"  defaultValue="25"/>
-
-            <label htmlFor="gravityRange">Gravity: <span id="gravityVal">0.001 </span> m/s2 </label>
-            <input type="range" id="gravityRange" min="0.0005" max="0.003" step="0.0005"  defaultValue="0.001"/>
+      <h1>Pizza Toss - CHALLENGE MODE</h1>
+      <h2>Looks like rats have taken over the kitchen! Toss them some pizzas to lure them out!</h2>
+      <ol className="directions">
+        Directions:
+        <li>Choose a starting velocity using the sliders</li>
+        <li>You can still change the mass of the pizza and the gravity if you would like</li>
+        <li>When all the rats have fallen off the platform, you win!</li>
+      </ol>
+      <ol className="questions">
+        Think like a scientist:
+        <li>Keep the y velocity constant and change the x velocity. How does the x velocity change your path?</li>
+        <li>Keep the x velocity constant and change the y velocity. How does the y velocity change the path?</li>
+        <li>Change both velocities. How can you best approach optimizing the path?</li>
+        <li>How do different magnitudes of gravity affect your path?</li>
+        <li>How can you tie the above information to the equations you know?</li>
+      </ol>
+      <div className="hintHelpContainer">
+        <div className="hintPopup">
+          <div
+            className={`hintButton ${showHint ? "active" : ""}`}
+            onMouseEnter={handleHintMouseEnter}
+            onMouseLeave={handleHintMouseLeave}
+          >
+            Hint
+          </div>
+          {showHint && (
+            <div className="hintText">
+              <ol className="hints">
+                 <li>Velocity is a vector! How do we get the sum of two vectors?</li>
+                 <li>Mass doesn't affect projectile motion, but size helps with knocking down more things!</li>
+                </ol>
+            </div>
+          )}
         </div>
-        <div id="playBox">
-            <button id="pizzaStart" onClick={Challenge}>Pizza Toss</button>
-            <p>Too difficult? Try the standard mode <Link to="/pizzatoss">here!</Link></p>
+        <div className="helpLink">
+          <Link to="/projectile-motion" style={{ textDecoration: "none", color: "inherit" }}>
+            Need More Help?
+          </Link>
         </div>
-    </div>)
-}
+      </div>
+      <div id="areaToRender"></div>
+      <div>
+        <label htmlFor="xvelocityRange">
+          X Velocity: <span id="xvelocityVal">10</span> m/s
+        </label>
+        <input type="range" id="xvelocityRange" min="10" max="40" step="5" defaultValue="15" />
+
+        <label htmlFor="yvelocityRange">
+          Y Velocity: <span id="yvelocityVal">10</span> m/s
+        </label>
+        <input type="range" id="yvelocityRange" min="1" max="40" step="5" defaultValue="15" />
+
+        <label htmlFor="massRange">
+          Mass: <span id="massVal">50</span> grams
+        </label>
+        <input type="range" id="massRange" min="1" max="50" step="10" defaultValue="25" />
+
+        <label htmlFor="gravityRange">
+          Gravity: <span id="gravityVal">0.001 </span> m/s2
+        </label>
+        <input type="range" id="gravityRange" min="0.0005" max="0.003" step="0.0005" defaultValue="0.001" />
+      </div>
+      <div id="playBox">
+        <button id="pizzaStart" onClick={Challenge}>
+          Pizza Toss
+        </button>
+        <p>
+          Too difficult? Try the standard mode <Link to="/pizzatoss">here!</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default PizzaChallenge;
--- my-app/src/Games/pizzaTossComponent.js
+++ my-app/src/Games/pizzaTossComponent.js
@@ -1,50 +1,99 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StartSlingshot } from "./pizzaToss";
 import { init } from "./pizzaToss";
 import { Link } from "react-router-dom";
 
 const PizzaToss = () => {
-    useEffect(() => {
-        window.onload=init();
-    }, [])
-    return (
+  const [showHint, setShowHint] = useState(false);
+
+  useEffect(() => {
+    window.onload = init();
+  }, []);
+
+  const handleHintMouseEnter = () => {
+    setShowHint(true);
+  };
+
+  const handleHintMouseLeave = () => {
+    setShowHint(false);
+  };
+
+  return (
     <div className="pizzaToss">
-        <h2>Pizza Toss</h2>
-        <h3>Looks like rats have taken over the kitchen! Toss them some pizzas to lure them out!</h3>
-        <ol>Directions: 
-            <li>Pull the pizza back and let go of the mouse to launch it</li>
-            <li>If you don't have enough velocity, the pizza won't launch</li>
-            <li>You can change the height of the toss and the mass/size of the pizza using the sliders below</li>
-            <li>When all the rats have fallen off the platform, you win!</li>
-        </ol>
-        <ol>Hints: 
-            <li>Pulling the pizza further back will increase its initial velocity</li>
-            <li>You can change the path of the pizza toss by changin the angle or the height you toss at</li>
-            <li>Mass doesn't affect projectile motion, but size helps with knocking down more things!</li>
-        </ol>
-        <ol className="questions">Think like a scientist:
-            <li>What happens to the path of the pizza when you pull back more? What about when you pull back less?</li>
-            <li>Challenge yourself! Try not to adjust the height too much and knock down the rats with a projectile path. Why would this be more difficult? </li>
-            <li>Try changing gravity. How does that affect the path of the projectile? Is it easier or harder at higher magnitudes of gravity, and why?</li>
-            <li>How can you tie the above information to the equations you know?</li>
-        </ol>
-        <div id="areaToRender"></div>
-        <div>
-            <label htmlFor="heightRange">Height: <span id="heightVal">210</span> m</label>
-
-            <input type="range" id="heightRange" min="130" max="400" step="1"  defaultValue="210"/>
-
-            <label htmlFor="massRange">Mass: <span id="massVal">50</span> grams</label>
-            <input type="range" id="massRange" min="10" max="50" step="10"  defaultValue="25"/>
-
-            <label htmlFor="gravityRange">Gravity: <span id="gravityVal">0.001 </span> m/s2 </label>
-            <input type="range" id="gravityRange" min="0.0005" max="0.003" step="0.0005"  defaultValue="0.001"/>
+      <h1>Pizza Toss</h1>
+      <h2>Looks like rats have taken over the kitchen! Toss them some pizzas to lure them out!</h2>
+      <ol className="directions">
+        Directions:
+        <li>Pull the pizza back and let go of the mouse to launch it</li>
+        <li>If you don't have enough velocity, the pizza won't launch</li>
+        <li>You can change the height of the toss and the mass/size of the pizza using the sliders below</li>
+        <li>When all the rats have fallen off the platform, you win!</li>
+      </ol>
+      <ol className="questions">
+        Think like a scientist:
+        <li>What happens to the path of the pizza when you pull back more? What about when you pull back less?</li>
+        <li>
+          Challenge yourself! Try not to adjust the height too much and knock down the rats with a projectile path. Why
+          would this be more difficult?
+        </li>
+        <li>
+          Try changing gravity. How does that affect the path of the projectile? Is it easier or harder at higher
+          magnitudes of gravity, and why?
+        </li>
+        <li>How can you tie the above information to the equations you know?</li>
+      </ol>
+      <div className="hintHelpContainer">
+        <div className="hintPopup">
+          <div
+            className={`hintButton ${showHint ? "active" : ""}`}
+            onMouseEnter={handleHintMouseEnter}
+            onMouseLeave={handleHintMouseLeave}
+          >
+            Hint
+          </div>
+          {showHint && (
+            <div className="hintText">
+                    <ol>
+                      <li>Pulling the pizza further back will increase its initial velocity</li>
+                      <li>You can change the path of the pizza toss by changing the angle or the height you toss at</li>
+                      <li>Mass doesn't affect projectile motion, but size helps with knocking down more things!</li>
+                    </ol>
+            </div>
+          )}
         </div>
-        <div id="playBox">
-            <button id="pizzaStart" onClick={StartSlingshot}>Pizza Toss</button>
-            <p>Too easy? Checkout the challenge mode <Link to="/pizzachallenge">here!</Link></p>
+        <div className="helpLink">
+          <Link to="/projectile-motion" style={{ textDecoration: "none", color: "inherit" }}>
+            Need More Help?
+          </Link>
         </div>
-    </div>)
-}
+      </div>
+      <div id="areaToRender"></div>
+      <div>
+        <label htmlFor="heightRange">
+          Height: <span id="heightVal">210</span> m
+        </label>
+        <input type="range" id="heightRange" min="130" max="400" step="1" defaultValue="210" />
+
+        <label htmlFor="massRange">
+          Mass: <span id="massVal">50</span> grams
+        </label>
+        <input type="range" id="massRange" min="10" max="50" step="10" defaultValue="25" />
+
+        <label htmlFor="gravityRange">
+          Gravity: <span id="gravityVal">0.001 </span> m/s2
+        </label>
+        <input type="range" id="gravityRange" min="0.0005" max="0.003" step="0.0005" defaultValue="0.001" />
+      </div>
+      <div id="playBox">
+        <button id="pizzaStart" onClick={StartSlingshot}>
+          Pizza Toss
+        </button>
+        <p className='challenge'>
+          Too easy? Checkout the challenge mode <Link to="/pizzachallenge">here!</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default PizzaToss;
